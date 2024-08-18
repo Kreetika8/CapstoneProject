706c30cc029bbd3fe4dca47bc8d6fd66edab30c9
Kreetika Bhetuwal <Kritikabhetuwal8@gmail.com>
2024-08-18 16:18:38 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import 'constants/app_constants.dart';
+import 'controllers/exports.dart';
+import 'views/ui/onboarding/onboarding_screen.dart';
+import 'views/common/exports.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => OnBoardNotifier()),
+        // ChangeNotifierProvider(create: (context) => LoginNotifier()),
+        // ChangeNotifierProvider(create: (context) => ZoomNotifier()),
+        // ChangeNotifierProvider(create: (context) => SignUpNotifier()),
+        ChangeNotifierProvider(create: (context) => JobsNotifier()),
+        // ChangeNotifierProvider(create: (context) => BookMarkNotifier()),
+        // ChangeNotifierProvider(create: (context) => ImageUploader()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      useInheritedMediaQuery: true,
+      designSize: const Size(375, 812), // Adjust design size as needed
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Capstone', // Correct capitalization
+          theme: ThemeData(
+            scaffoldBackgroundColor: Color(kLight.value),
+            iconTheme: IconThemeData(color: Color(kDark.value)),
+            primarySwatch: Colors.grey,
+          ),
+          home: const OnboardingScreen(),
+        );
+      },
+    );
+  }
+}
